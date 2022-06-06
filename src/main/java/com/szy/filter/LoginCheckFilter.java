27bea983169a10e0e672b0cf1d3a0f1a93ac9aa0
DDYnb <935505038@qq.com>
2022-06-06 17:56:26 +0800

    添加Swagger

--- src/main/java/com/szy/filter/LoginCheckFilter.java
+++ src/main/java/com/szy/filter/LoginCheckFilter.java
@@ -37,7 +37,11 @@ public class LoginCheckFilter implements Filter {
                 "/backend/**",
                 "/front/**",
                 "/user/sendMsg",
-                "/user/login"
+                "/user/login",
+                "/doc.html",
+                "/webjars/**",
+                "/swagger-resources",
+                "/v2/api-docs"
         };
         //如果请求uri不需要过滤，则直接放行
         if(check(uris, requestURI)){
